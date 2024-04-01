5d6af942abf19429a49a729ec6ad5239cc0d961b
beckgo34 <beckgo34@naver.com>
2024-04-01 09:16:17 +0900

    project

---
+++ project/src/main/java/com/icia/project/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.icia.project.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	public String home() {
+		return "index";
+	}
+}
